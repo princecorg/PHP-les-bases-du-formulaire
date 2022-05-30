851b7e37a7edc726dd48861bebffc7d2d63c0692
princecorg <cvallot76@gmail.com>
2022-05-30 14:56:27 +0200

    Ajout d'un fichier formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,6 @@
+<!-- Création d'un formulaire -->
+<form action="" method="post">
+    <label for="nom"> Nom </label><input type="text" name="nom" placeholder="saisissez votre nom">
+    <label for="prenom"> Nom </label><input type="text" name="prenom"placeholder="saisissez votre prénom">
+    <button type="submit">Valider</button>
+</form>
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo 'Bonjour et bienvenue'; ?>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon site</title>
+</head>
+
+<body>
+    <?php echo 'Bonjour et bienvenue'; ?>
+</body>
+
+</html>
